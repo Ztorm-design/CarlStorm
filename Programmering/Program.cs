009786619aa429304e28cfd85c029e48855521ce
Arvid Sandblom <LargerPizza@gmail.com>
2024-09-19 11:13:00 +0200

    Add project files.

---
+++ Programmering/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+  class Program
+{
+    static void Main(string[] args)
+    {
+        // Type your username and press enter
+        Console.WriteLine("Enter username:");
+
+        // Create a string variable and get user input from the keyboard and store it in the variable
+        string Carl = Console.ReadLine();
+
+        // Print the value of the variable (userName), which will display the input value
+        Console.WriteLine("Username is: " + Carl);
+    }
+}
